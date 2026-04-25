308addecbfc9737f57cf7a5d5629b2d43dc0c327
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-24 19:06:38 -0600

    Set current working directory of SW-logbook to the directory of the current campaing

--- dm-log-campaign.el
+++ dm-log-campaign.el
@@ -120,7 +120,7 @@
   "Open or refresh the *SW-Logbook* buffer."
   (let ((buf (get-buffer-create dm-log-buffer-name)))
     (with-current-buffer buf
-      (setq default-directory dm-log--current-campaign)
+      (setq default-directory (file-name-directory dm-log--current-logbook-file))
       (dm-log-mode)
       (dm-log-ui--render dm-log--current-logbook-file))
     (pop-to-buffer buf)
